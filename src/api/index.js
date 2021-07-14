e2807bdca4f0c764268dac7573f0a906cc037982
Alejandro <aleangrodriguez123@gmail.com>
2021-07-13 22:56:01 -0500

    feat: add constants and api endpoints for search and discover movies

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,15 @@
 import client from './axios';
 
-export const discoverMovies = () => {
-}
+import {
+    DISCOVER_MOVIES_ENDPOINT,
+    SEARCH_MOVIES_ENDPOINT,
+} from '../constants';
+
+
+export const discoverMovies = (popularity = 'popularity.desc') => 
+    client.get(`${DISCOVER_MOVIES_ENDPOINT}?sort_by=${popularity}`)
+        .then(response => response.data);
+
+export const searchMovies = (query = '') => 
+    client.get(`${SEARCH_MOVIES_ENDPOINT}?query=${query}`)
+        .then(response => response.data);
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+
+// endpoints
+export const DISCOVER_MOVIES_ENDPOINT = '/discover/movie';
+export const SEARCH_MOVIES_ENDPOINT = '/search/search-movies';
