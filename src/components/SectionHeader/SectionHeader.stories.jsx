252bc3303ae13f20914b6fd95ebdc04192789fcc
Alejandro <aleangrodriguez123@gmail.com>
2021-07-15 07:42:15 -0500

    feat: add font and section header

---
+++ src/components/SectionHeader/SectionHeader.stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { SectionHeader } from '.';
+
+export default {
+  component: SectionHeader,
+  title: 'Components/SectionHeader',
+}
+
+export const Default = () => 
+  <SectionHeader 
+    title={'Popular Movies'}
+  />;
--- src/components/SectionHeader/index.jsx
+++ src/components/SectionHeader/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styled from 'styled-components';
+import { FaFire } from 'react-icons/fa';
 
-export default function index() {
+const StyledParagraph = Styled.p`
+    font-family: 'Roboto';
+`;
+
+const StyledContainer = Styled.div`
+    display: flex;
+`;
+
+const StyledIconContainer = Styled.div`
+    align-self: center;
+`;
+
+
+export const SectionHeader = ({ title, iconSrc }) => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <StyledContainer>
+            <StyledIconContainer>
+                <FaFire />
+            </StyledIconContainer>
+            <StyledParagraph>
+                {title}
+            </StyledParagraph>
+        </StyledContainer>
+    );
+};
+
+SectionHeader.propTypes = {
+    title: PropTypes.string.isRequired,
+    iconSrc: PropTypes.string.isRequired,
+};
