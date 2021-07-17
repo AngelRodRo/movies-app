7c63d4d884b718e98b908424d84637b8dc81a286
Angel <aleangrodriguez123@gmail.com>
2021-07-16 20:09:41 -0500

    feat: add layout to app

--- src/App.js
+++ src/App.js
@@ -3,28 +3,21 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
+import Layout from './layout';
 import Home from './views/Home';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
+      <Layout>
         <Switch>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
+      </Layout>
     </Router>
   );
 }
