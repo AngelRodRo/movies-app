1df587bd87e1f932feccf60432ad00cc2ddfdf8a
Angel <aleangrodriguez123@gmail.com>
2021-07-16 20:09:11 -0500

    chore: add background for main section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Styled from 'styled-components';
+
+import { SearchInput } from '../components/SearchInput';
+
+const MainSection = Styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+
+    justify-items: center;
+    align-items: center;
+
+    flex-direction: column;
+
+    background: url('images/main-background.jpeg');
+`;
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <MainSection>
+            <SearchInput />
+        </MainSection>
     );
 };
 
