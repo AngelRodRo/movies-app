120ad10d15a5931102ca1f5dc58c6fe15b8a4ef7
Angel <aleangrodriguez123@gmail.com>
2021-07-18 11:58:26 -0500

    feat: add search feature

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styled from 'styled-components';
+import { useHistory } from 'react-router';
 
-import { SearchInput } from '../components/SearchInput';
+import { SearchInput } from 'components/SearchInput';
 
 const MainSection = Styled.div`
     display: flex;
     width: 100%;
     height: 100vh;
+
+    padding-top: 250px;
 
     justify-items: center;
     align-items: center;
@@ -17,9 +20,33 @@ const MainSection = Styled.div`
 `;
 
 const Home = () => {
+    const history = useHistory();
+    const [query, setQuery] = useState('');
+
+    const ENTER_KEY = 13;
+
+    const search = async () => {
+        history.push(`/search?q=${query}`);
+    };
+
+    const startSearch = (event) => {
+        if (event.keyCode === ENTER_KEY) {
+            search();
+        }
+    };
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    }
+
     return (
         <MainSection>
-            <SearchInput />
+            <SearchInput
+                placeholder="Search for a movie ..."
+                onKeyUp={startSearch}
+                value={query}
+                onChange={handleChange}
+            />
         </MainSection>
     );
 };
