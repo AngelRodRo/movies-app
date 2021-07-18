4e3f5e52c8196bfd30868ec7b13d41c897ef243a
Angel <aleangrodriguez123@gmail.com>
2021-07-18 11:55:32 -0500

    feat: change poster size

--- src/components/Poster/index.jsx
+++ src/components/Poster/index.jsx
@@ -3,21 +3,22 @@ import Styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledPoster = Styled.div`
-    width: 200px;
-    height: 350px;
+    width: 300px;
+    height: 450px;
 
     background: url('${props => props.src}');
     background-position: center;
 `;
 
-export const Poster = ({ 
+export const Poster = ({
     src,
     onClick
 }) => {
+    const fullPath = `https://image.tmdb.org/t/p/w300${src}`;
     return (
         <StyledPoster
             onClick={onClick}
-            src={src}
+            src={fullPath}
         />
     );
 };
